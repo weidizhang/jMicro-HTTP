a82712219f1bd8ea810c5b3e22fae0774d729e3e
Weidi Zhang <weidiz999@yahoo.com>
2016-07-09 16:48:26 +0800

    Bump version to v1.0.1

--- src/io/github/weidizhang/jmicrohttp/HTTPServer.java
+++ src/io/github/weidizhang/jmicrohttp/HTTPServer.java
@@ -14,7 +14,7 @@ import com.sun.net.httpserver.HttpServer;
 public class HTTPServer {
 	
 	private HttpServer httpServer;
-	private static String version = "1.0.0";
+	private static String version = "1.0.1";
 	
 	public HTTPServer(int port, String directory, boolean enableDirListing) {		
 		try {
